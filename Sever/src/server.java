8af298d8028022ac3769a533becab7ebe531233d
hana-kr <51320682+hana-kr@users.noreply.github.com>
2019-07-14 14:14:50 +0430

    Add files via upload

---
+++ Sever/src/server.java
@@ -0,0 +1,49 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+
+public class server extends Application {
+   public static DataInputStream dis ;
+   public static DataOutputStream dos ;
+
+    static Stage stage;
+
+
+    // static
+    @Override
+    public void start(Stage Stage) throws Exception {
+        stage = Stage;
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Stage.setScene(new Scene(root, 600, 600));
+        Stage.setTitle("Messanger");
+        stage.alwaysOnTopProperty();
+        Stage.show();
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket serverSocket = new ServerSocket(8088);
+        Socket socket ;
+        socket = serverSocket.accept();
+        dos =new DataOutputStream(socket.getOutputStream());
+        dis = new DataInputStream(socket.getInputStream());
+
+        launch();
+
+
+
+        }
+
+    }
+
